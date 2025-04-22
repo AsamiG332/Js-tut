e6a791e8525fcddff9262c6dbd5e92236720871f
AsamiG332 <malikgloria2019@gmail.com>
2025-04-22 15:14:04 +0100

    first js class

---
+++ java.js
@@ -0,0 +1,45 @@
+// let challenge='30 Days Challenge Of JavaScript';
+// console.log(challenge);
+// let challenge='30 Days Challenge Of JavaScript';
+// console.log(challenge.length);
+// let challenge='30 Days Challenge Of JavaScript';
+// console.log(challenge.toUpperCase())
+// let challenge='30 days Challenge Of JavaScript';
+// console.log(challenge.toLowerCase())
+// let challenge='30 Days Challenge Of JavaScript';
+// console.log(challenge.substr (0, 2));
+// let challenge='30 Days Of JavaSCRIPT';
+// console.log(challenge.substr(3,18))
+// let challenge='30 Days Of JavaScript';
+// console.log(challenge.includes('Script'));
+// let challenge='30 Day Of JavaScript';
+// console.log(challenge.split(' '))
+// let challenge='Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon';
+// console.log(challenge.split(','))
+// let challenge='30 Days Of JavaScript';
+// console.log(challenge.replace('JavaScript', 'Python'))
+// let challenge='30 Days Of JavaScript';
+// console.log(challenge.charAt(15))
+// let challenge='30 Days Of JavaScript';
+// console.log(challenge.charCodeAt('J'))
+// let challenge='30 Days Of JavaScript';
+// console.log(challenge.indexOf('a'))
+// let challenge='30 Days Of JavaScript';
+// console.log(challenge.lastIndexOf('a'))
+// let challenge='You cannot end a sentence with because because because is a conjunction';
+// console.log(challenge.indexOf('because'))
+// let challenge='you cannot end a sentence with because because because is a conjunction';
+// console.log(challenge.lastIndexOf('because'))
+// let challenge='You cannot end a sentence with because because because is a conjunction';
+// console.log(challenge.search('because'))
+// let challenge='  30 Days Of JavaScript  '
+// console.log(challenge.trim(' '))    
+// let challenge='30 Days Of JavaScript';
+// console.log(challenge.startsWith(30))
+// 
+// let challenge='30 Days Of JavaScript';
+// console.log(challenge.match('a'))
+// let challenge='30 Days Of JavaScript';
+// console.log(challenge.concat('Days', 'of', 'JavaScript'))
+let challenge='30 Days Of JavaScript';
+console.log(challenge.repeat(2))
