3d9902723d2a409f945dd92fe0d044687063f451
AsamiG332 <malikgloria2019@gmail.com>
2025-04-29 23:43:01 +0100

    Boolean.js

---
+++ boolean.js
@@ -0,0 +1,147 @@
+console.log(typeof('10')==(typeof(10)))
+
+//  a= 10 , b= '10'
+//  c= a+b
+//  console.log(c);
+// c = a + parseInt(b)
+// console.log(c);
+
+// console.log(parseFloat('9.8')==(10))
+
+// a='9.8', b=10
+// c=a+b
+// console.log(c);
+// c= a + parseFloat(b)
+// console.log(parseFloat('9.8'))      
+// console.log(Number('9.8'))
+// console.log(Math.round(parseFloat('9.8')))
+
+// let on=' python and jargon';
+// console.log(on.includes('python'))
+// console.log(on.includes('jargon'))
+
+// let jargon='I hope this course is not full of jargon';
+// console.log(jargon.includes('jargon'))
+
+// let random=Math.random()
+// let number=random *101
+// console.log(number)
+// let randomNumRoundToFloor=Math.floor(number)
+// console.log(randomNumRoundToFloor)
+
+// let random=Math.random()
+// let number=random *51
+// console.log(number)
+// let randomNumRoundToFloor=Math.floor(number)
+// console.log(randomNumRoundToFloor + 50)
+
+// let random=Math.random()
+// let number=random *251
+// console.log(number)
+// let randomNumRoundToFloor=Math.floor(number)
+// console.log(randomNumRoundToFloor)
+
+// let word='JavaScript';
+// console.log(word.length)
+
+// let random=Math.random()
+// let number=random * word.length+1
+// console.log(number)
+// let randomNumRoundToFloor=Math.floor(number)
+// console.log(randomNumRoundToFloor)
+// console.log(word[randomNumRoundToFloor])
+// console.log('1\t1\t1\t1\t1\n2\t1\t2\t4\t8\n3\t1\t3\t9\t27\n4\t1\t4\t16\t64\n5\t1\t5\t25\t125');
+
+// let phrase='You cannot end a sentence with because because because is a conjunction';
+// let start= phrase.indexOf('because because because');
+// let result=phrase.substr(start,'because because because'.length);
+
+// let name='Grace';
+// let lastname='Darasimi';
+// let country='Nigeria';
+// let city='Abuja';   
+// let age=31;
+// let ismarried=false;
+// let year=2024;
+
+// console.log(typeof name);
+// console.log(typeof lastname);
+// console.log(typeof country);
+// console.log(typeof city);
+// console.log(typeof age);
+// console.log(typeof ismarried);
+// console.log(typeof year);
+
+// console.log('10'===10);
+// console.log(parseInt('9.8')===10);
+
+// const Text = "hello";
+// const Number = 123;
+// const Object = {};
+// console.log(Boolean(Text));
+// console.log(Boolean(Number));
+// console.log(Boolean(Object));
+
+// const nothing=null;
+// const number=0;
+// const zero='';
+
+// console.log(Boolean(nothing));
+// console.log(Boolean(number));
+// console.log(Boolean(zero));
+
+
+
+// console.log( 4 > 3);
+// console.log(4 >= 3);
+//  console.log(4 < 3);
+ 
+// console.log(4 <= 3);
+// console.log(4 == 4);
+// console.log(4 === 4);
+// console.log(4 != 4);
+// console.log(4 !== 4);
+// console.log(4 != '4');
+// console.log(4 == '4');
+// console.log(4 === '4');
+
+
+// const pythonlength='python';
+// const jargonlength='jargon';
+
+// console.log(pythonlength.length);
+// console.log(jargonlength.length);
+// console.log(pythonlength < jargonlength);
+
+// console.log(4 > 3 && 10 < 12);
+// console.log(4 > 3 && 10 > 12);
+// console.log(4 > 3 || 10 < 12);
+// console.log( 4 > 3 || 10 > 12);
+// console.log(!(4 > 3));
+// console.log(!(4 < 3));
+// console.log(!(false));
+// console.log(!(4 > 3 && 10 < 12));
+// console.log(!(4 > 3 && 10 > 12));
+// console.log(!(4 === '4'));
+
+// const numbers=[33,2,8];
+// numbers.sort();
+// console.log(numbers[1]);
+// console.log(1 + "2" + "2");
+// console.log(typeof NaN);
+
+// const word1 = "dragon";
+// const word2 = "python";
+// const searchString = "on";
+// const noOnInDragon = !word1.includes(searchString);
+// const noOnInPython = !word2.includes(searchString);
+// const statementIsTrue = noOnInDragon && noOnInPython;
+// console.log("Statement: There is no 'on' in both dragon and python");
+// console.log("Is it true? " + statementIsTrue);
+
+// let now= new Date(). getFullYear();
+// console.log(now);
+
+let now=new Date(). getMonth();
+console.log(now);
+        
